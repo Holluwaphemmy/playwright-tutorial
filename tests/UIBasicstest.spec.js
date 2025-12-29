b969feeea069131a8b628decf12df134d07077cb
Femi Gideon <femi.gideon@sterling.ng>
2025-12-29 16:50:28 +0100

    first commit

---
+++ tests/UIBasicstest.spec.js
@@ -0,0 +1,13 @@
+const { test } = require("@playwright/test");
+
+test("Browser Context Playwright test", async ({ browser }) => {
+  const context = await browser.newContext();
+  const page = await context.newPage();
+  await page.goto("https://rahulshettyacademy.com/LoginpagePractise/");
+  console.log(await page.title())
+});
+
+test("Page Playwright test", async ({ page }) => {
+  await page.goto("https://google.com/");
+   console.log(await page.title())
+});
